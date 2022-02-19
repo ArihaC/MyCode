865126faaf2d5a5e458755ce01b871e564d7f87d
ArihaC <100046354+ArihaC@users.noreply.github.com>
2022-02-19 15:11:20 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var box;
+
+function setup() {
+  createCanvas(800,400);
+  box = createSprite(400,200,20,20);
+  box.velocityX = 4
+
+}
+
+function draw() 
+{
+  background(30);
+drawSprites();
+}
+
+
+
+
